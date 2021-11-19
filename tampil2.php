bdf0f35a4fea18fce2be52d4c1808cb9d0eb1730
ilhamnr1610 <91207165+ilhamnr1610@users.noreply.github.com>
2021-11-19 10:32:30 +0700

    Update tampil2.php

--- tampil2.php
+++ tampil2.php
@@ -17,12 +17,26 @@
                     <td style='width:50%; text-align: center; border: 1px solid black;'>Penulis<br>$baris[2]</td>
                     <td style='width:50%; text-align: center; border: 1px solid black;'>Jenis Buku<br>$baris[3]</td>
                 </tr><tr>
-                    <td style='width:50%; text-align: center; border: 1px solid black;'><a href='ubah_data.php?id_buku=$baris[0]'>Ubah &nbsp</a></td>
-                    <td style='width:50%; text-align: center; border: 1px solid black;'><a href='hapus.php?id_buku=$baris[0]'>Hapus</a></td>
+                    <td style='width:50%; text-align: center; border: 1px solid black;'>
+                    <a href='ubah_data.php?id_buku=$baris[0]'>Ubah &nbsp</a></td>
+                    <td style='width:50%; text-align: center; border: 1px solid black;'>
+                    <a href='hapus.php?id_buku=$baris[0]'>Hapus</a></td>
                 </tr>
             </table><br><br><br>";
         $a++;}
-    echo "<a href='formulir.php'><h1 style='text-align: center;'>Tambah data</h1></a>";
+        
+    echo "<form action = 'search.php' method='post'>
+            <table style='width:100%;text-align: center;'>
+                <tr>
+                    <td style='width:50%;text-align: center;'>
+                        <a href='formulir.php'><h3 style='text-align: center;'>Tambah data</h3></a>
+                    </td>
+                    <td style='width:50%;text-align: center;'>
+                        <h3>Cari Buku</h3><input type='text' name ='judul_buku'>
+                        <input type='submit' name='search' value='cari'>
+                    </td>
+                </tr>
+            </table>
+            </form>";
     $conn->close();
 ?>
-
